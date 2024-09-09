9c82d0aabfa466886ac0df2596bf69b8111a9721
Carlos Eduardo Leite de Oliveira <carlosoliveirahl13@gmail.com>
2024-09-09 15:02:41 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [ 
+    {
+        titulo: "Engenharia da Computação",
+        descricao: "A Engenharia da Computação é um curso completo que te prepara para um mercado de trabalho promissor e desafiador. Se você gosta de tecnologia, de resolver problemas e de criar soluções inovadoras, essa pode ser a área ideal para você.",
+        link: "https://vestibular.unisal.br/cursos-campinas/engenharia-de-computacao/?gad_source=1&gclid=Cj0KCQjwiuC2BhDSARIsALOVfBLRmz_wbw0BYOLI2BATumXV0TgGK4oUoQ5mgXVRvN9mw5lMZSWUSNMaAjyDEALw_wcB",
+        tags: "engenharia computação tecnologia"
+    },
+    {
+        titulo: "Engenharia de Software",
+        descricao: "A Engenharia de Software foca no desenvolvimento de softwares, desde a concepção até a implementação. É ideal para quem gosta de programar e criar soluções personalizadas.",
+        link: "https://www.puc-campinas.edu.br/graduacao/engenharia-de-software/",
+        tags: "engenharia software"
+    },
+    {
+        titulo: "Tecnologia da Informação",
+        descricao: "A Tecnologia da Informação abrange um leque mais amplo, incluindo hardware, software, redes e infraestrutura. É perfeita para quem busca uma formação completa e versátil.",
+        link: "https://vestibular.unisal.br/cursos-campinas/tecnologia-em-analise-e-desenvolvimento-de-sistemas/?gad_source=1&gclid=CjwKCAjwreW2BhBhEiwAavLwfDCAPpjqBRQGK3kdlKR6Y6CPrtJMn5TpSGQfzCHDQz7_cNfd5GK6vBoCKxkQAvD_BwE",
+        tags: "tecnologia informação hardware" 
+    }
+];
